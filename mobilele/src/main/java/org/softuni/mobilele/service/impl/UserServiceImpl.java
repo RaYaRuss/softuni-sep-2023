aab051611679dbd22b7b3dae66c4278ba6ddb65e
Radina Yaneva <110345586+RaYaRuss@users.noreply.github.com>
2023-09-27 14:49:31 +0300

    Login user implementation done

--- mobilele/src/main/java/org/softuni/mobilele/service/impl/UserServiceImpl.java
+++ mobilele/src/main/java/org/softuni/mobilele/service/impl/UserServiceImpl.java
@@ -1,14 +1,38 @@
 package org.softuni.mobilele.service.impl;
 
 import org.softuni.mobilele.model.dto.UserRegistrationDto;
+import org.softuni.mobilele.model.entity.UserEntity;
+import org.softuni.mobilele.repository.UserRepository;
 import org.softuni.mobilele.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
-    public void registerUser(UserRegistrationDto userRegistrationDto) {
-        System.out.println(userRegistrationDto);
+    public void registerUser(
+            UserRegistrationDto userRegistrationDto) {
+
+        userRepository.save(map(userRegistrationDto));
+    }
+
+    private UserEntity map(UserRegistrationDto userRegistrationDTO) {
+        return new UserEntity()
+                .setActive(true)
+                .setFirstName(userRegistrationDTO.firstName())
+                .setLastName(userRegistrationDTO.lastName())
+                .setEmail(userRegistrationDTO.email())
+                .setPassword(passwordEncoder.encode(userRegistrationDTO.password()));
     }
 }
