e8bd2e68ecc651e495223703ba764b5fe517df8f
Radina Yaneva <110345586+RaYaRuss@users.noreply.github.com>
2023-10-02 22:31:48 +0300

    Next features added

---
+++ mobilele/src/main/java/org/softuni/mobilele/service/OfferService.java
@@ -0,0 +1,11 @@
+package org.softuni.mobilele.service;
+
+import org.softuni.mobilele.model.dto.CreateOfferDTO;
+
+import java.util.UUID;
+
+public interface OfferService {
+
+    UUID createOffer(CreateOfferDTO createOfferDTO);
+
+}
